6c2112cbec1fb855941564b2058f1a7e0289f38f
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-21 10:19:40 +0900

    [feature]js counter redux로 구현전 js로 counter만들기

--- src/index.js
+++ src/index.js
@@ -2,9 +2,29 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./Component/App";
 
-ReactDom.render(
-  <div>
-    <App />
-  </div>,
-  document.getElementById("app")
-);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const display = document.querySelector("span");
+let number = 0;
+
+function numberOnDisplay() {
+  display.innerHTML = number;
+}
+function addOne() {
+  number += 1;
+  numberOnDisplay();
+}
+function minusOne() {
+  number -= 1;
+  numberOnDisplay();
+}
+add.addEventListener("click", addOne);
+minus.addEventListener("click", minusOne);
+
+// ReactDom.render(
+//   <div>
+//     <App />
+//   </div>,
+//   document.getElementById("app")
+// );
+//
