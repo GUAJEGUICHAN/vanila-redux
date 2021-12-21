5252f0962a723465d295220577c58a126b5a282a
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-21 11:01:00 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -5,26 +5,27 @@ const minus = document.getElementById("minus");
 const display = document.querySelector("span");
 let number = 0;
 
-function reducer() {
-  return "hello";
+const InitialState = {
+  number: 1,
+  word: ""
+};
+function reducer(state = InitialState, action) {
+  const { number } = state;
+  if (action.type === "add") {
+    return {
+      ...state,
+      number: number + 1
+    };
+  }
+  return state;
 }
 
 const store = createStore(reducer);
 
-console.log(store);
-console.log(store.getState());
-
-function numberOnDisplay() {
-  display.innerHTML = number;
-}
-function addOne() {
-  number += 1;
-  numberOnDisplay();
-}
-function minusOne() {
-  number -= 1;
-  numberOnDisplay();
-}
-
-add.addEventListener("click", addOne);
-minus.addEventListener("click", minusOne);
+add.addEventListener("click", () => {
+  console.log(store.getState());
+  store.dispatch({
+    type: "add"
+  });
+  console.log(store.getState());
+});
